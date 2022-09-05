0f057f4c791c9e0955159fd3543e1515b6f89aee
ikhsanulfiqrii <112652414+ikhsanulfiqrii@users.noreply.github.com>
2022-09-05 21:58:19 +0700

    Add files via upload

---
+++ SDA_pratugas.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+public class SDA_pratugas {
+    public static void main(String[] args) {
+        String[]kata = {"Teknik","Informatika"};
+        int b=1;
+        if(b==1){
+            for (int a=0; a<5;a++){
+                System.out.println(kata[0]);
+                System.out.println(kata[1]);
+            }
+        } else {
+            System.out.print("ulangi");
+        }
+    }
+}
